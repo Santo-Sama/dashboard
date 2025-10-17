b2e3f93f93db20d4d83002ea3d40c75d8d517d78
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-10-17 10:59:26 +0200

    inseriti path base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './Component/dashboard/dashboard';
+import { Login } from './Component/login/login';
+import { register } from 'module';
+import { Register } from './Component/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: Dashboard}
+    {path: 'login' , component: Login}
+    {path: 'register' , component: Register}
+];
