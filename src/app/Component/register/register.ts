d9af18488efbd7980fb7a78d5742380954788f4c
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-10-21 13:29:58 +0200

    aggiunto register

--- src/app/Component/register/register.ts
+++ src/app/Component/register/register.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule , MatButtonModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
+    onSubmit(form: NgForm){
+    const email = form.value.email
+    const password = form.value.password
+    // chiamare authservice
+  }
 }
