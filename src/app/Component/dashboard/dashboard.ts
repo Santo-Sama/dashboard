791ca15b465fff78e2dc4fc0e3a141ece55532c5
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-10-21 14:22:08 +0200

    aggiunta dashboard

--- src/app/Component/dashboard/dashboard.ts
+++ src/app/Component/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, RouterLink, RouterOutlet],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,17 @@ import { Dashboard } from './Component/dashboard/dashboard';
 import { Login } from './Component/login/login';
 import { register } from 'module';
 import { Register } from './Component/register/register';
+import { Pagina1 } from './Component/pagina1/pagina1';
+import { Pagina2 } from './Component/pagina2/pagina2';
+import { Pagina3 } from './Component/pagina3/pagina3';
 
 export const routes: Routes = [
-    {path: '' , component: Dashboard},
+    {path: '' ,  component: Dashboard, children:[
+        {path: '' , redirectTo: 'pagina1' , pathMatch: 'full'},
+        {path: 'pagina1' , component: Pagina1 },
+        {path: 'pagina2' , component: Pagina2 },
+        {path: 'pagina3' , component: Pagina3 },
+]},
     {path: 'login' , component: Login},
     {path: 'register' , component: Register},
 ];
